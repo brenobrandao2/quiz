987c8c113fe177504da98a328b18ff6897ad7d24
brenobrandao2 <breno.brandao@prodap.com.br>
2021-10-15 17:20:16 -0300

    fix: Altera urls

--- src/utils/baseUrls.js
+++ src/utils/baseUrls.js
@@ -3,5 +3,5 @@ const dev_server = 'https://137.184.132.242'
 const localhost = 'http://localhost'
 
 export const server = dev_server
-export const base_url_client_quiz = `${server}:9002/`
-export const base_url_db = `${server}:3001`
+export const base_url_client_quiz = 'https://quiz.brenobrandao.site/'
+export const base_url_db = `${server}:8443`

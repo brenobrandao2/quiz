b52fde5764f1f081767d640be958421b847c9056
brenobrandao2 <breno.brandao@prodap.com.br>
2021-10-15 11:35:41 -0300

    feature: Add dev server

--- src/utils/baseUrls.js
+++ src/utils/baseUrls.js
@@ -1,6 +1,7 @@
 const lam_server = 'https://159.203.187.163'
-const dev_server = 'http://localhost'
+const dev_server = 'https://137.184.132.242'
+const localhost = 'http://localhost'
 
-export const server = lam_server
+export const server = dev_server
 export const base_url_client_quiz = `${server}:9002/`
 export const base_url_db = `${server}:3001`

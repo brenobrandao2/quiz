366c62bb719dbb2392f52e68c5c3d8ec0f22c36a
brenobrandao2 <breno.brandao@prodap.com.br>
2021-10-17 11:08:55 -0300

    feature: Adiciona favicon

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import UsersList from './pages/UsersList';
 import CreateUser from './pages/CreateUser';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { useUser } from './contexts/AuthContext';
 
 function App() {
@@ -19,19 +20,26 @@ function App() {
   if (user) {
     console.log('Logged in')
     return (
-      <Router>
-        <div className="App">
-          <Header />
-          <Route path="/" exact component={QuizList} />
-          <Route path="/create-quiz" exact component={CreateQuiz} />
-          <Route path="/create-question" exact component={CreateQuestion} />
-          <Route path="/create-final-card" exact component={CreateFinalCard} />
-          <Route path="/answers-configuration" exact component={AnswersConfiguration} />
-          <Route path="/configurations" exact component={Configurations} />
-          <Route path="/users" exact component={UsersList} />
-          <Route path="/create-user" exact component={CreateUser} />
-        </div>
-      </Router>
+      <HelmetProvider>
+        <Router>
+          <div className="App">
+            <Helmet>
+              <meta charSet="utf-8" />
+              <link rel="icon" type="image/png" href="https://lifeandmoney.com.br/wp-content/uploads/2021/04/Favicon-LM-300x300.png" />
+              <title>Life + Money Quiz</title>
+            </Helmet>
+            <Header />
+            <Route path="/" exact component={QuizList} />
+            <Route path="/create-quiz" exact component={CreateQuiz} />
+            <Route path="/create-question" exact component={CreateQuestion} />
+            <Route path="/create-final-card" exact component={CreateFinalCard} />
+            <Route path="/answers-configuration" exact component={AnswersConfiguration} />
+            <Route path="/configurations" exact component={Configurations} />
+            <Route path="/users" exact component={UsersList} />
+            <Route path="/create-user" exact component={CreateUser} />
+          </div>
+        </Router>
+      </HelmetProvider>
     );
   } else {
     console.log('Not logged in')

e4dec678d8a29f2990242602c8621afb18359d54
brenobrandao2 <breno.brandao@prodap.com.br>
2022-02-08 20:45:22 -0300

    fix: Aponta urls para lifeandmoney

--- src/utils/baseUrls.js
+++ src/utils/baseUrls.js
@@ -1,6 +1,6 @@
-const lam_server = 'https://quiz-adm.pontosemilhas.com/quiz-adm-service'
+const lam_server = 'https://quiz-adm.lifeandmoney.com.br/quiz-adm-service'
 const dev_server = 'https://brenobrandao.site/quiz-adm-service'
 const local_server = 'http://localhost:3001'
 
-export const base_url_client_quiz = 'https://cartoes.pontosemilhas.com/'
+export const base_url_client_quiz = 'https://cartoes.lifeandmoney.com.br/'
 export const base_url_db = lam_server
